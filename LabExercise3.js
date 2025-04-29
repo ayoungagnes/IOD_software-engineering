fbcf13b8d11122c5a6869cd809a1b34e91e623d8
ayoungagnes <87073933+ayoungagnes@users.noreply.github.com>
2025-04-29 14:49:55 +1000

    Lab Exercise 3 (including Extension 1,2)

---
+++ LabExercise3.js
@@ -0,0 +1,12 @@
+function changme1(){
+    document.getElementById("column1").style.backgroundColor='violet';
+    document.getElementById("heading1").innerHTML = "Bye bye";
+    var x = document.getElementById("myText1").value;
+    document.getElementById("heading1").innerHTML = x;
+}
+function changme2(){
+    document.getElementById("column2").style.backgroundColor='white';
+    document.getElementById("heading2").innerHTML = "For now"
+    var x = document.getElementById("myText2").value;
+  document.getElementById("heading2").innerHTML = x;
+}
