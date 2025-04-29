6a60fd3d7fc8707a7a4c88654831b79c81e7040b
ayoungagnes <87073933+ayoungagnes@users.noreply.github.com>
2025-04-29 22:20:24 +1000

    Update LabExercise3.js

--- LabExercise3.js
+++ LabExercise3.js
@@ -1,10 +1,10 @@
-function changme1(){
+function changeme1(){
     document.getElementById("column1").style.backgroundColor='violet';
     document.getElementById("heading1").innerHTML = "Bye bye";
     var x = document.getElementById("myText1").value;
     document.getElementById("heading1").innerHTML = x;
 }
-function changme2(){
+function changeme2(){
     document.getElementById("column2").style.backgroundColor='white';
     document.getElementById("heading2").innerHTML = "For now"
     var x = document.getElementById("myText2").value;
