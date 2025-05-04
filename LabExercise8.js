bcf4ac76e7c32eb124450dd3d37bbb850fb20855
ayoungagnes <ayagnes07@gmail.com>
2025-05-04 21:40:13 +1000

    fix argument

--- LabExercise8.js
+++ LabExercise8.js
@@ -1,6 +1,19 @@
 
-        function rolldice() {
+function rolldice() {
         let dicetype = document.getElementById("dice").value;
         let result =  Math.floor(Math.random() * dicetype) + 1;
         document.getElementById("result").innerHTML = "You rolled: " + result; 
-        }
+}
+
+//Create the roll dice function by passing an argument, in order to reutilize the same function multiple times
+function argRolldice(dicetype) {
+        let result =  Math.floor(Math.random() * dicetype) + 1;
+        document.getElementById("result").innerHTML = "You rolled: " + result;
+}    
+
+//      argRolldice(6) 
+if(document.getElementById("result").innerHTML == "You rolled: 8") {
+        throw new Error('Test failed');
+
+}
+        
