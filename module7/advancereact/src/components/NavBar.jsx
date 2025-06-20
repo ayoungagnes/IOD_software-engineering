ec3d27261b78669cab6972a6d015177024706f04
ayoungagnes <ayagnes07@gmail.com>
2025-06-20 17:22:21 +1000

    revised module7 lab work5

--- module7/advancereact/src/components/NavBar.jsx
+++ module7/advancereact/src/components/NavBar.jsx
@@ -1,12 +1,27 @@
 import { NavLink } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function NavBar() {
-    return(
-        <>
-            <NavLink to="/">Home</NavLink>
-        {" "} <NavLink to="/LoginForm">Login</NavLink>{" "} 
-        <NavLink to="/BitcoinRates">BitcoinRates</NavLink>
-        </>
-    )
+  return (
+    <AppBar>
+      <Toolbar>
+        <Stack direction="row" spacing={2}>
+          <Button component={NavLink} to="/" color="inherit">
+            Home
+          </Button>
+          <Button component={NavLink} to="/LoginForm" color="inherit">
+            Login
+          </Button>
+          <Button component={NavLink} to="/BitcoinRates" color="inherit">
+            BitcoinRates
+          </Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
 }
-export default NavBar; 
+
+export default NavBar;
